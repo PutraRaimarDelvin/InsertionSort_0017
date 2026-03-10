e935178dffa24ef1bc34ad9b3b5e208c15609d3f
PutraRaimarDelvin <delvinputra805@gmail.com>
2026-03-10 11:35:07 +0700

    menambahkan fungsi insertion sort

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -30,6 +30,25 @@ void input() {
     }
 }
 
+void insertionsort(){
+    int temp;
+    int j;
+
+    for (int i = 1; i < n; i++) {
+        temp = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = temp;
+    }
+}
+
 int main() {
     input();
+    insertionsort();
 }
