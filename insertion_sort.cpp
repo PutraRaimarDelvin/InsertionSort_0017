4dd1983ada3f490d91cfc7bb3be882de1d187991
PutraRaimarDelvin <delvinputra805@gmail.com>
2026-03-10 11:34:46 +0700

    menambahkan input elemen array

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -17,6 +17,17 @@ void input() {
             cout << "\nArray yang anda masukkan maksimal 20 elemen.\n";
         }
     }
+
+    cout << endl;
+    cout << "================================================" << endl;
+    cout << "===========Masukkan Element Array===============" << endl;
+    cout << "================================================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
 }
 
 int main() {
