d71fcfa833340d94c3da3f162558f3e507de5223
PutraRaimarDelvin <delvinputra805@gmail.com>
2026-03-10 11:34:24 +0700

    menambahkan fungsi input jumlah data array

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -4,6 +4,21 @@ using namespace std;
 int arr[20];
 int n;
 
-int main() {
+void input() {
+    while (true)
+    {
+        cout << "Masukkan Jumlah Data pada Array : ";
+        cin >> n;
 
+        if (n <= 20) {
+            break;
+        }
+        else {
+            cout << "\nArray yang anda masukkan maksimal 20 elemen.\n";
+        }
+    }
+}
+
+int main() {
+    input();
 }
